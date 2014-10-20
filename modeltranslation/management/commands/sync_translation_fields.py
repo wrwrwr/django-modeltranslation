402b42009228d40df2b7af98ff2da3232d4f4b46
wrwrwr <git@wr.waw.pl>
2014-10-20 18:12:53 +0200

    Slightly refactored get_missing_languages method of the sync_translated_field command.

--- modeltranslation/management/commands/sync_translation_fields.py
+++ modeltranslation/management/commands/sync_translation_fields.py
@@ -46,7 +46,7 @@ class Command(NoArgsCommand):
             for field_name, fields in opts.local_fields.items():
                 field = list(fields)[0]
                 column_name = field.db_column if field.db_column else field_name
-                missing_langs = list(self.get_missing_languages(column_name, db_table))
+                missing_langs = self.get_missing_languages(column_name, db_table)
                 if missing_langs:
                     found_missing_fields = True
                     if self.verbosity > 0:
@@ -89,14 +89,17 @@ class Command(NoArgsCommand):
         db_table_desc = self.introspection.get_table_description(self.cursor, db_table)
         return [t[0] for t in db_table_desc]
 
-    def get_missing_languages(self, field_name, db_table):
+    def get_missing_languages(self, column_name, db_table):
         """
-        Gets only missings fields.
+        Returns codes of languages for which the given field does not have a
+        translation field.
         """
+        missing_langs = []
         db_table_fields = self.get_table_fields(db_table)
         for lang_code in AVAILABLE_LANGUAGES:
-            if build_localized_fieldname(field_name, lang_code) not in db_table_fields:
-                yield lang_code
+            if build_localized_fieldname(column_name, lang_code) not in db_table_fields:
+                missing_langs.append(lang_code)
+        return missing_langs
 
     def get_sync_sql(self, field_name, missing_langs, model):
         """
