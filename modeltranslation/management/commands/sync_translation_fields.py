aeb27b0b81cb9d4db598798ff8e75b1cd57d022c
wrwrwr <git@wr.waw.pl>
2014-10-20 17:58:24 +0200

    Removed comments without substance in sync_translated_fields.

--- modeltranslation/management/commands/sync_translation_fields.py
+++ modeltranslation/management/commands/sync_translation_fields.py
@@ -32,9 +32,6 @@ class Command(NoArgsCommand):
     )
 
     def handle_noargs(self, **options):
-        """
-        Command execution.
-        """
         self.cursor = connection.cursor()
         self.introspection = connection.introspection
         self.interactive = options['interactive']
@@ -47,7 +44,6 @@ class Command(NoArgsCommand):
             model_full_name = '%s.%s' % (model._meta.app_label, model._meta.module_name)
             opts = translator.get_options_for_model(model)
             for field_name, fields in opts.local_fields.items():
-                # Take `db_column` attribute into account
                 field = list(fields)[0]
                 column_name = field.db_column if field.db_column else field_name
                 missing_langs = list(self.get_missing_languages(column_name, db_table))
@@ -115,7 +111,6 @@ class Command(NoArgsCommand):
             f = model._meta.get_field(new_field)
             col_type = f.db_type(connection=connection)
             field_sql = [style.SQL_FIELD(qn(f.column)), style.SQL_COLTYPE(col_type)]
-            # column creation
             stmt = "ALTER TABLE %s ADD COLUMN %s" % (qn(db_table), ' '.join(field_sql))
             if not f.null:
                 stmt += " " + style.SQL_KEYWORD('NOT NULL')
